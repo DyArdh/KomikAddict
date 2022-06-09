10b0d7b4df3e50267bc8bf9024ade2b1df82eac5
DyArdh <rizuna30@gmail.com>
2022-06-09 16:39:12 +0700

    edit homepage

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,30 +1,36 @@
 <div class="container mt-5">
     <div class="row">
-        <div class="col-12 col-md-9 d-grid">
-            <button class="btn btn-primary filter-show" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                Button with data-bs-target
-            </button>
-            <div class="collapse" id="collapseExample">
-                <div class="card card-body">
-                    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore esse eligendi ducimus sunt pariatur corporis tempora ut, voluptatibus quis blanditiis, dicta iste deleniti aut? Reprehenderit nemo quae pariatur blanditiis sapiente?
-                    Nihil possimus necessitatibus molestiae cumque! Impedit doloremque reprehenderit illo totam voluptatibus, at pariatur, itaque quod, autem quibusdam suscipit ex laudantium odio rem! Praesentium perferendis aliquid magni sed accusantium quos dolorum?
-                    Harum corporis atque sequi voluptate tempore voluptatibus dolores quas modi cupiditate autem adipisci sint maxime fuga rerum accusantium cum consectetur doloremque, est dolor iusto? Facere dolorem maiores quod aut corporis?
-                    Facere error laboriosam vero, quae molestias excepturi porro est ipsam provident dicta, voluptatum qui harum consectetur. Atque dolorum nulla aut incidunt, ducimus dolor sequi corporis voluptates nisi sed officiis quas.
-                    Autem placeat facere minus nam ea dolorem molestiae obcaecati repellat. Incidunt unde similique magnam laboriosam consequatur eos numquam hic officiis. Tempora nisi, iste cumque debitis et voluptatum cum qui officia?
-                    Aut, eaque? Dolore, sunt accusantium sed cupiditate consequatur quasi quis, reiciendis minima et iure voluptatem nostrum est velit beatae voluptates accusamus dolor provident atque aut tempora inventore recusandae culpa animi?
-                    Quia accusantium a dolore suscipit quis commodi quam consequuntur. Aliquam vitae assumenda ipsa, eum expedita deserunt, sint facere doloremque hic excepturi accusantium itaque, delectus dolorem. Labore ipsa nemo veritatis debitis.
-                    Earum totam assumenda necessitatibus, reprehenderit quibusdam, cumque molestiae ad iste ea voluptate autem nobis veniam dolores atque dolorum nemo voluptatibus ex laudantium consectetur corporis numquam corrupti dignissimos. Quia, earum minus!
-                    Dolore officiis veniam aut est adipisci ut nesciunt eius sapiente illum dicta vitae dolorum, consequatur id nisi accusamus? Reiciendis odio sit animi eos voluptatem magni rerum. Facilis vero quae voluptates?
-                    Quibusdam a quas laboriosam exercitationem dolorum repellendus perspiciatis officiis iusto nemo voluptas, saepe reprehenderit culpa, libero, excepturi nostrum ut recusandae? Sed voluptate molestias repellat doloribus, ex hic saepe vel alias!
+        <div class="col-12 col-md-9">
+            <div class="row">
+                <div class="col-12 d-grid">
+                <a class="btn btn-primary text-left px-4 fw-bold" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    Filter
+                </a>
+                <div class="collapse" id="collapseExample">
+                    <div class="card card-body">
+                        <label for="search" class="pb-2">Search :</label>
+                        <input type="text" class="form-control" id="search" name="search">
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-12">
+                    <div class="card px-4 py-4">
+                        <div class="header-content d-flex justify-item-center">
+                            <h6 class="fw-bold">Daftar Komik</h6>
+                        </div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cumque nemo modi voluptatibus maxime, officiis incidunt neque id tenetur accusantium, enim laborum eveniet nulla minima quod labore sapiente asperiores aliquam!
+                    Libero ipsum nulla non veniam eos alias at laborum, consectetur exercitationem officia neque a laudantium distinctio quidem id quod, cumque pariatur ratione voluptate ad, nesciunt illum odio in architecto. Illo!
+                    Doloremque enim ad nihil rem rerum quos harum blanditiis culpa corrupti veniam voluptatum iure debitis ullam quae explicabo reprehenderit possimus voluptas doloribus maxime repellendus, eius dolor deleniti. Qui, iusto eveniet.
+                    Animi adipisci molestias accusantium eveniet hic qui molestiae consectetur facere odio maxime! Placeat, reprehenderit. Accusantium quisquam aperiam voluptas, deserunt sapiente commodi ex harum facilis. Fugiat reprehenderit adipisci amet quod quae!
+                    Incidunt rerum porro pariatur veritatis facere. Sapiente expedita dolorum totam eaque numquam provident, voluptatum hic velit. Quas assumenda obcaecati voluptate, ad iusto impedit asperiores culpa. Vel voluptatibus delectus at aperiam?
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-3 sidebar">
+        <div class="col-md-3 sidebar text-light">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. ur dolorum, dolore fugit deserunt soluta culpa porro facere architecto sequi voluptatem ipsa eum laboriosam delectus nesciunt cupiditate. Velit ratione laborum officiis alias iusto!
-        </div>
-    </div>
-    <div class="row mt-5">
-        <div class="col-12">
         </div>
     </div>
 </div>
