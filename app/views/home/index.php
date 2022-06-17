4e855482348ceeaf7d1e4fcca7309a9668fc824d
Jastin Theo Hariyono <72338131+OrSSY@users.noreply.github.com>
2022-06-17 17:20:45 +0700

    masuk

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,31 +1,65 @@
 <div class="container mt-5">
     <div class="row">
-        <div class="col-12 col-md-9">
-            <div class="container">
-                <div class="col">
-                    <form>
-                        <input type="text" placeholder="find comic">
-                    </form>
+        <div class="col-xs-12 col-md-9">
+            <div class="card lessblack text-center">
+                <div class="card-header">
+                  <h4 class="fw-bold">Latest Review</h4>
                 </div>
-                <div class="col">
-                    <h1>tes filter</h1>
-                </div>
-
-
-                <!-- <div class="col-12 d-grid">
-                    <a class="btn btn-primary text-left px-4 fw-bold" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                        Filter
-                    </a>
-                    <div class="collapse" id="collapseExample">
-                        <div class="card card-body">
-                            <label for="search" class="pb-2">Search :</label>
-                            <input type="text" class="form-control" id="search" name="search">
+                <div class="card-body">
+                    <div class="row">
+                    <div class="col-xs-12 col-md-4">
+                    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+                        <div class="carousel-indicators">
+                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                         </div>
+                        <div class="carousel-inner">
+                          <div class="carousel-item active">
+                            <a class="stretched-link" href="index.php"></a>
+                            <img src="<?=base_url?>/assets/img/spyxfamily.jpg" class="card-img-top" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                              <h5 style="background-color: rgba(27, 27, 27, 0.641);">First slide label</h5>
+                            </div>
+                          </div>
+                          <div class="carousel-item">
+                            <a class="stretched-link" href="index.php"></a>
+                            <img src="<?=base_url?>/assets/img/spyxfamily.jpg" class="card-img-top" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                              <h5 style="background-color: rgba(27, 27, 27, 0.641);">Second slide label</h5>
+                            </div>
+                          </div>
+                          <div class="carousel-item">
+                            <a class="stretched-link" href="index.php"></a>
+                            <img src="<?=base_url?>/assets/img/spyxfamily.jpg" class="card-img-top" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                              <h5 style="background-color: rgba(27, 27, 27, 0.641);">Third slide label</h5>
+                            </div>
+                          </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Next</span>
+                        </button>
+                      </div>
                     </div>
-                </div> -->
-            </div>
-            <div class="row mt-3">
-                <div class="col-12">
+                    <div class="col-md-8">
+                        <h5 class="card-title text-light" style="text-align: left;">Welcome to KomikAddict</h5>
+                        <p class="card-text" style="text-align: justify;"><b>KomikAddict </b> adalah sebuah Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et risus gravida, facilisis purus vitae, sollicitudin eros. Curabitur id dolor pellentesque, efficitur nisl non, tempor felis. Vivamus at fringilla sapien.Suspendisse et risus gravida, facilisis purus vitae, sollicitudin eros..</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-footer text-muted">
+                  2 days ago
+                </div>
+              </div>
+            <div class="row mt-2">
+                <div class="col-xs-12">
                     <div class="card px-4 py-4 lessblack">
                         <div class="header-content d-flex justify-item-center">
                             <h4 class="fw-bold" style="color: var(--text-color-light);">Daftar Komik</h4>
@@ -68,12 +102,27 @@
                                 </div>
                             </div>
                         </div>            
-                    </div>
+                        </div>
                     </div>
                 </div>
             </div>
         <div class="col-md-3 sidebar text-light">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. ur dolorum, dolore fugit deserunt soluta culpa porro facere architecto sequi voluptatem ipsa eum laboriosam delectus nesciunt cupiditate. Velit ratione laborum officiis alias iusto!
+            <div class="card lessblack">
+                <h5 class="card-header">Featured</h5>
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card my-2 lessblack">
+                <h5 class="card-header">Featured</h5>
+                <div class="card-body">
+                  <h5 class="card-title">Special title treatment</h5>
+                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <!-- FontAwesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- local css -->
     <link rel="stylesheet" href="<?= base_url; ?>/assets/css/main-style.css">
   </head>
   <body>
@@ -18,7 +19,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #191c25;">
   <div class="container">
-    <a class="navbar-brand" href="#" style="font-weight: bold;font-size: 30px;color: #fff;">NoName</a>
+    <a class="navbar-brand" href="#" style="font-weight: bold;font-size: 30px;color: #fff;">KomikAddict</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
       <span class="navbar-toggler-icon"><i class="fa-solid fa-bars" style="color: #fff;"></i></span>
     </button>
